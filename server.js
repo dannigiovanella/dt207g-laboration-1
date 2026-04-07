52a061722215c5de1f1bb43e9941f3037932da0a
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-04-07 15:56:07 +0200

    Lagt till validering av inputfält och skapat SQL-insert för kurs-tabellen i

--- server.js
+++ server.js
@@ -35,6 +35,35 @@ cvApp.get("/add", async (request, response) => {
     response.render("add");
 })
 
+cvApp.post("/add", async (request, response) => {
+  const { coursecode, coursename, progression, syllabus } = request.body;
+
+  if (!coursecode || !coursename || !progression || !syllabus) {
+    return response.render("add", {
+      error: "Fyll i alla fält",
+      old: request.body
+    });
+  }
+
+  try {
+    await client.query(
+      "INSERT INTO courses (coursecode, coursename, progression, syllabus) VALUES ($1, $2, $3, $4)",
+      [coursecode, coursename, progression, syllabus]
+    );
+    response.redirect("/");
+  } 
+  catch (err) {
+    console.log("DB error", err);
+
+    response.render("add", {
+      error: "Något gick fel när kursen sparades",
+      old: request.body
+    });
+  }
+});
+
+
+
 
 //Starta server
 cvApp.listen(process.env.PORT, () => {
